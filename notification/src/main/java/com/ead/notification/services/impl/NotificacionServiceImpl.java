23751d52d60b6bb3848fba06b17165cbe2148e14
alex villa <alex22_w@gmail.com>
2023-08-07 15:10:08 -0500

    Preparar Asynchronous Communication via Commands em Notification Microservice

---
+++ notification/src/main/java/com/ead/notification/services/impl/NotificacionServiceImpl.java
@@ -0,0 +1,16 @@
+package com.ead.notification.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.ead.notification.repositories.NotificationRepository;
+import com.ead.notification.services.NotificacionService;
+
+@Service
+public class NotificacionServiceImpl implements NotificacionService {
+
+	final NotificationRepository notificationRepository;
+	
+	public NotificacionServiceImpl(NotificationRepository notificationRepository) {
+		this.notificationRepository=notificationRepository;
+	}
+}
