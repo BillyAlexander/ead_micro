a67a891283b8be3e72f80dc85fa7b0083e7c8ae2
alex villa <alex22_w@gmail.com>
2023-08-29 11:30:24 -0500

    Access Token Pattern in Notification Microservice

--- notification/src/main/java/com/ead/notification/controllers/UserNotificationController.java
+++ notification/src/main/java/com/ead/notification/controllers/UserNotificationController.java
@@ -11,6 +11,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +27,6 @@ import com.ead.notification.services.NotificacionService;
 
 
 @RestController
-//@RequestMapping("/notifications")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class UserNotificationController {
 	
@@ -35,13 +36,16 @@ public class UserNotificationController {
 		this.notificacionService=notificacionService;
 	}
 	
+	@PreAuthorize("hasAnyRole('STUDENT')")
 	@GetMapping("/users/{userId}/notifications")
 	public ResponseEntity<Page<NotificationModel>> getAllNotificationByUser(@PathVariable(value = "userId") UUID userId,
-			@PageableDefault(page = 0,size = 10,sort = "notificationId", direction = Sort.Direction.ASC) Pageable pageable){
+			@PageableDefault(page = 0,size = 10,sort = "notificationId", direction = Sort.Direction.ASC) Pageable pageable,
+			Authentication authentication){
 		
 		return ResponseEntity.status(HttpStatus.OK).body(notificacionService.findAllNotificationByUser(userId,pageable));
 	}
 	
+	@PreAuthorize("hasAnyRole('STUDENT')")
 	@PutMapping("/users/{userId}/notifications/{notificationId}")
 	public ResponseEntity<Object> getAllNotificationByUser(@PathVariable(value = "userId") UUID userId,
 			@PathVariable(value = "notificationId") UUID notificationId,
