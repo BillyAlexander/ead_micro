1ca720083ccb5cdd28e23cc7a90749faaa6f1561
alex villa <alex22_w@gmail.com>
2023-09-22 09:34:12 -0500

    Spring 3 MS Configs ApiGateway, ServiceRegistry, ConfigServer

--- config-server/src/main/java/com/ead/configserver/configs/WebSecurityConfig.java
+++ config-server/src/main/java/com/ead/configserver/configs/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.ead.configserver.configs;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,14 +23,10 @@ public class WebSecurityConfig  {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.
-		httpBasic().
-		and()
-		.authorizeRequests()
-		.anyRequest().authenticated()
-		.and()
-		.csrf().disable()
-		.formLogin();
-		
+		httpBasic(Customizer.withDefaults())
+		.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+		.csrf(crsf-> crsf.disable())
+		.formLogin(Customizer.withDefaults());		
 		return http.build();
 	}
 	
